cefc1e4a38c7b0ab0c7c7551ef72ebf8a403e386
Nayem Uddin <nurakib.nstu@gmail.com>
2016-10-29 23:01:44 +0600

    a % b finding

---
+++ Big Mod using String.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+long bigmod(string num, long m){
+    long i, rem;
+    for(i = 0, rem = 0; num[i]; i++){
+        rem = rem * 10 + (num[i] - '0');
+        rem = rem % m;
+    }
+    return rem;
+}
+int main(){
+    long m;
+    string s;
+    cin >> s >> m;
+    cout << bigmod(s, m) << endl;
+    return 0;
+}
